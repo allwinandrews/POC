5d1fe4a56776d32cca35e419cf57ef95799264c6
allwinjandrews <allwin@labglo.com>
2020-02-13 15:10:09 +0530

    Adds location feature which updates as user changes location in Add Labour Screen

--- src/components/layout/AppLayout.js
+++ src/components/layout/AppLayout.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Footer, FooterTab, Button, Text} from 'native-base';
 import {Alert} from 'react-native';
 import firebase from 'react-native-firebase';
+// import RNLocation from 'react-native-location';
 
 export default function AppLayout({navigation}) {
+  // useEffect(() => {
+  //   RNLocation.requestPermission({
+  //     ios: 'whenInUse',
+  //     android: {
+  //       detail: 'coarse',
+  //     },
+  //   }).then(granted => {
+  //     console.log('Location access: ', granted);
+  //   });
+  // }, []);
   const logout = async () => {
     await firebase
       .auth()
